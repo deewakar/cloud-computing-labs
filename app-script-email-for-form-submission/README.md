58d2ca40d07d19a60ae0e1c7aa9151ff4034bc7a
dwagle <diwakar.wagle@gmail.com>
2023-06-27 22:16:45 +0545

    Fixed naming convention; relocate comments to the README file

--- app-script-email-for-form-submission/README.md
+++ app-script-email-for-form-submission/README.md
@@ -1,1 +1,19 @@
-# cloud-computing-labs
+# cloud-computing Lab 1
+
+## Lab activity Steps
+1. Create a google form inside your Google Drive
+
+2. Setup the form so that it collects email addresses by default
+
+    1. goto Settings -> responses -> collect email address and select 'Responder input'
+
+    2. do the same in the Settings -> default -> Form defaults section
+
+3. Go to Responses tab and click on 'view in sheets', a google sheet with the responses opens
+4. In the google sheet, click on extensions -> App Scripts, an app script editor opens
+5. Write the onSubmit() function (in appscript.js file) and run the script, allow required permissions
+6. Click on 'triggers' menu on the left sidebar and create a new trigger for the onSubmit function,
+   1.    put source as the google sheet
+   2.  put event as 'on form submit' and click on submit
+
+Now whenever user submits the form, an email is sent to the given email address in the form
--- app-script-email-for-form-submission/appscript.js
+++ app-script-email-for-form-submission/appscript.js
@@ -1,18 +1,5 @@
-// 1. Create a google form inside google drive
-// 2. Setup the form so that it collects email addresses by default, 
-//     goto Settings -> responses -> collect email address and select 'Responder input'
-//     do the same in the Settings -> default -> Form defaults section
-// 3. Go to Responses tab and click on 'view in sheets', a google sheet with the responses opens
-// 4. In the google sheet, click on extensions -> App Scripts, an app script editor opens
-// 5. Write the onSubmit() function as shown below and run the script, allow required permissions
-// 6. Click on 'triggers' menu on the left sidebar and create a new trigger for the onSubmit function,
-//       put source as the google sheet
-//       put event as 'on form submit' and click on submit
-// Now whenever user submits the form, an email is sent to the given email address in the form
-
-
 function onSubmit(e){
-  const myemail = Session.getActiveUser().getEmail();
+  const myEmail = Session.getActiveUser().getEmail();
   const email = e.namedValues['Email Address'][0];
 
 
